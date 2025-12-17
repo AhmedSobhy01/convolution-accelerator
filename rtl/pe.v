f1a6dc577b8463c63f69ead7f911ca773f998328
habibayman <habibaayman2004@outlook.com>
2025-12-17 19:38:12 +0200

    fix: remove unused reg

--- rtl/pe.v
+++ rtl/pe.v
@@ -9,13 +9,11 @@ module pe #(parameter DATA_WIDTH = 32, parameter INPUT_WIDTH = 8)
     output wire [INPUT_WIDTH-1:0] out_down,
     output wire [INPUT_WIDTH-1:0] out_right
 );
-    reg [DATA_WIDTH-1:0] partial_sum;
     reg [INPUT_WIDTH-1:0] top_reg;
     reg [INPUT_WIDTH-1:0] left_reg;
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            partial_sum   <= 0;
             top_reg       <= 0;
             left_reg      <= 0;
         end else if (load_kernel_signal) begin
