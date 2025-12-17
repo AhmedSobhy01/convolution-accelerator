f9355f930a12fc526a46a659a7df9dd24a95f5fc
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-12-17 21:20:02 +0200

    feat: add config and memory wait states
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- control_unit/control_unit.v
+++ control_unit/control_unit.v
@@ -1,6 +1,7 @@
 module control_unit #(
-    parameter CYCLES_PER_KERNEL_LOAD = 16,
-    parameter FILL_CYCLES = 16
+    parameter CYCLES_PER_KERNEL_LOAD = 16, // From DRAM to SRAM
+    parameter FILL_CYCLES = 16,             // Number of cycles to fill the systolic array
+    parameter SA_DIM = 8                    // Size of one dimension of the systolic array
 )(
     input  wire clk,
     input  wire rst_n,
@@ -11,11 +12,11 @@ module control_unit #(
     output reg  done,
 
     // Input data stream from DRAM to Data Loader
-    output reg  rx_ready,
-    input  wire rx_valid,
-    // Output data stream from Systolic Array to DRAM
-    input wire tx_ready,
+    output reg  rx_ready, // ready to receive from DRAM
+    input  wire rx_valid, // DRAM has valid data
 
+    // Output data stream from Systolic Array to DRAM
+    input wire tx_ready,  // DRAM ready to receive data
 
     output wire [5:0] dl_cfg_N,
     output wire [3:0] dl_cfg_K,
@@ -24,7 +25,7 @@ module control_unit #(
     output reg start_loading_image_to_sram,
 
     output reg load_kernel,
-    output reg [1:0] kernel_index,
+    output reg [1:0] kernel_index, // Index of the current kernel being loaded (for K > SA_SIZE)
 
     output reg load_column,
     output reg [5:0] load_column_index,
@@ -53,7 +54,11 @@ module control_unit #(
     reg [3:0] state;
 
     integer counter;
-    // reg [1:0] kernel_index = 2'd0;
+    
+    reg [1:0] kernel_index = 2'd0;
+    reg kernel_bigger_than_sa;       // Flag to indicate if K > SA_SIZE
+    reg [2:0] total_kernel_parts;    // Total number of kernel parts to load (for K > SA_SIZE)
+    reg [1: 0] tiles_per_dim;        // Number of tiles per dimension when K > SA_SIZE (K / SA_DIM)
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
@@ -67,12 +72,16 @@ module control_unit #(
             systolic_data_valid <= 1'b0;
             start_accumlation <= 1'b0;
             load_kernel <= 1'b0;
-            kernel_index <= 32'd0;
+            kernel_index <= 2'd0;
+            kernel_bigger_than_sa <= 1'b0;
 
         end else begin
             
             load_kernel <= 1'b0;
             systolic_data_valid <= 1'b0;
+            start_loading_image_to_sram <= 1'b0;
+            start_loading_kernel_to_sram <= 1'b0;
+            start_accumlation <= 1'b0;
 
             case (state)
                 IDLE: begin
@@ -81,6 +90,35 @@ module control_unit #(
                         state <= CONFIG;
                     end
                 end
+
+                CONFIG: begin
+                    // Check if kernel size is bigger than systolic array size
+                    kernel_bigger_than_sa <= (cfg_K > SA_DIM) ? 1'b1 : 1'b0;
+                    
+                    if (cfg_K > SA_DIM) begin
+                        tiles_per_dim <= (cfg_K + SA_DIM - 1) / SA_DIM; // Ceiling division
+                        total_kernel_parts <= tiles_per_dim * tiles_per_dim; // Ceiling division
+                    end else begin
+                        total_kernel_parts <= 3'd1;
+                    end
+
+                    state <= WAIT_MEM;
+                end 
+
+                WAIT_MEM: begin
+                    rx_ready <= 1'b1;
+                    if (rx_valid) begin
+                        start_loading_kernel_to_sram <= 1'b1;
+                        state <= LOAD_K_TO_SRAM;
+                        counter <= 0;
+                    end
+                end  
+
+                LOAD_K_TO_SRAM: begin
+                    
+
+                end
+
                 LOAD_K_TO_SA: begin
                     load_kernel <= 1'b1;
                     kernel_index <= 2'd0;
