9e943582cc265aba07df70620ca012c3e156d567
habibayman <habibaayman2004@outlook.com>
2025-12-18 00:27:05 +0200

    feat: implement sa shift register

---
+++ rtl/shift_register.v
@@ -0,0 +1,34 @@
+module shift_register #(parameter SHIFT_SIZE = 4, DATA_WIDTH = 32)
+(
+    input wire clk,
+    input wire rst,
+    input wire [(SHIFT_SIZE*DATA_WIDTH)-1:0] in_data,
+    output wire [(SHIFT_SIZE*DATA_WIDTH)-1:0] out_data
+);
+
+    localparam X = SHIFT_SIZE*DATA_WIDTH*(SHIFT_SIZE-1);
+    reg [(SHIFT_SIZE*SHIFT_SIZE*DATA_WIDTH)-1:0] shift_reg;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            shift_reg <= 0;
+        end else begin
+            shift_reg <= {shift_reg[X-1:0], in_data};
+        end
+    end
+
+    // 0, 4, 8, ...
+    // 0*SHIFT_SIZE + 0*DATA_WIDTH
+    // 1*SHIFT_SIZE + 1*DATA_WIDTH
+    // 2*SHIFT_SIZE + 2*DATA_WIDTH
+    integer k, idx;
+    always @(*) begin
+        for (k = 0; k < SHIFT_SIZE; k = k + 1) begin
+            idx = k*SHIFT_SIZE*DATA_WIDTH + k*DATA_WIDTH;
+            shift_reg[idx +: DATA_WIDTH] = in_data[k*DATA_WIDTH +: DATA_WIDTH];
+        end
+    end
+
+    assign out_data = shift_reg;
+
+endmodule
