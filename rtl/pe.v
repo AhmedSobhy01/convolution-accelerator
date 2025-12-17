05ba7ce28fdd3b8e2c9e25dca61948c762d6291f
habibayman <habibaayman2004@outlook.com>
2025-12-17 19:51:00 +0200

    refactor: remove useless extra regs & conditions

--- rtl/pe.v
+++ rtl/pe.v
@@ -7,33 +7,24 @@ module pe #(parameter DATA_WIDTH = 32, parameter INPUT_WIDTH = 8)
     input wire [INPUT_WIDTH-1:0] in_kernel,
     output wire [DATA_WIDTH-1:0] out_psum,
     output wire [INPUT_WIDTH-1:0] out_input,
-    output wire [INPUT_WIDTH-1:0] out_kernel,
+    output wire [INPUT_WIDTH-1:0] out_kernel
 );
     reg [DATA_WIDTH-1:0] partial_sum;
-    reg [INPUT_WIDTH-1:0] kernel_reg;
     reg [INPUT_WIDTH-1:0] top_reg;
     reg [INPUT_WIDTH-1:0] left_reg;
-    reg kernel_loaded;
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             partial_sum   <= 0;
-            kernel_reg    <= 0;
             top_reg       <= 0;
             left_reg      <= 0;
-            kernel_loaded <= 0;
         end else if (load_kernel_signal) begin
-            if (!kernel_loaded && (in_kernel != 0)) begin
-                kernel_reg    <= in_kernel;
-                kernel_loaded <= 1;
-            end
             top_reg     <= in_input;
             left_reg    <= in_kernel;
-            partial_sum <= partial_sum + kernel_reg * in_input;
+            partial_sum <= partial_sum + left_reg * top_reg;
         end else begin
-            partial_sum   <= partial_sum + kernel_reg * in_input;
             top_reg       <= in_input;
-            kernel_loaded <= 0;
+            partial_sum   <= partial_sum + left_reg * top_reg;
         end
     end
 
